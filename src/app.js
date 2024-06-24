0948cc14a15fbb455b4d36ab118b811761ea0c09
sayyadmehraj <sayyadmehraj01@gmail.com>
2024-06-24 14:26:21 +0530

    There was problem in LogOut section

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ app.use(express.json({limit:"16Kb"}))
 app.use(express.urlencoded({extended:true,limit:"16Kb"}))
 app.use(express.static("public")) //This allows to store some of the user data into the server
 
+app.use(cookieParser())
 //routes declaration Generally it is a standarad to declare the routes here
 import userRouter from "./routes/user.routes.js"
 
