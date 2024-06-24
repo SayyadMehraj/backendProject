506ff1b28ce489c8697bba305e11ece92f5f8984
sayyadmehraj <sayyadmehraj01@gmail.com>
2024-06-24 13:56:15 +0530

    routes setup is done

--- src/app.js
+++ src/app.js
@@ -13,4 +13,12 @@ app.use(express.json({limit:"16Kb"}))
 app.use(express.urlencoded({extended:true,limit:"16Kb"}))
 app.use(express.static("public")) //This allows to store some of the user data into the server
 
+//routes declaration Generally it is a standarad to declare the routes here
+import userRouter from "./routes/user.routes.js"
+
+app.use("/api/v1/users",userRouter)
+
+//When ever it is directed to 
+//http://localhost:port/api/v1/users/register
+
 export { app }
